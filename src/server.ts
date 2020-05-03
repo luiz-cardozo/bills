632a67c930cdc23e9604bf705e81c5026b2960e5
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-05-03 18:30:54 -0300

    initial structure configured

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333');
 });
